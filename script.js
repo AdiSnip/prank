b95fe2ab772851e83eee4a944ac665f5338e2194
Aditya Singh <87600915+AdiSnip@users.noreply.github.com>
2024-07-17 12:06:25 +0530

    main files

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let list = ["tu chutia hai na","tu noob hai na?","tu single hai na"]
+let c = 0;
+let question = document.querySelector(".question")
+let buttons = document.querySelectorAll(".button")
+let no = document.querySelector("#no")
+let yes = document.querySelector("#yes")
+buttons.forEach((button)=>{
+    button.addEventListener("mouseover",(event)=>{
+        if (button.innerText === "No"){
+            no.style.position = "absolute"
+            let a = Math.floor(Math.random() * window.innerWidth)
+            let b = Math.floor(Math.random() * window.innerHeight)
+            no.style.left = `${Math.min(a, window.innerWidth - no.clientWidth)}px`;
+            no.style.top = `${Math.min(b, window.innerHeight - no.clientHeight)}px`;
+        }
+    })
+})
+yes.addEventListener("click",()=>{
+    alert("😊😂🤣😁🤣🤣")
+    if (c<list.length){
+        question.innerText = `${list[c]}`
+        c++
+    }
+})
